abb936b3164d65bffbd0a7cf2fc098fe95af861b
wsw <wushenwen@hzyushi.cn>
2016-12-23 14:55:19 +0800

    添加静态库

---
+++ MySDK.framework/Headers/MySDK.h
@@ -0,0 +1,21 @@
+//
+//  MySDK.h
+//  MySDK
+//
+//  Created by wsw on 2016/12/21.
+//  Copyright © 2016年 wsw. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import <MySDK/MySDKTest.h>
+
+//! Project version number for MySDK.
+FOUNDATION_EXPORT double MySDKVersionNumber;
+
+//! Project version string for MySDK.
+FOUNDATION_EXPORT const unsigned char MySDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MySDK/PublicHeader.h>
+
+
---
+++ MySDK.framework/Headers/MySDKTest.h
@@ -0,0 +1,15 @@
+//
+//  MySDKTest.h
+//  MySDK
+//
+//  Created by wsw on 2016/12/21.
+//  Copyright © 2016年 wsw. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MySDKTest : NSObject
+
++ (void)printTest;
+
+@end
